7c5c21438e1b7401fd622bf21d82f6a114246a83
milo <milo@localhost>
2026-07-25 23:30:28 -0400

    Bootstrap Qt6 application

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_odexplorer_FILE /home/milo/Projects/odexplorer-linux/build/odexplorer)
+set(__QT_DEPLOY_TARGET_odexplorer_TYPE EXECUTABLE)
---
+++ src/ui/MainWindow.cpp
@@ -0,0 +1,9 @@
+#include "MainWindow.hpp"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    resize(1280, 720);
+
+    setWindowTitle("OD Explorer Linux");
+}
