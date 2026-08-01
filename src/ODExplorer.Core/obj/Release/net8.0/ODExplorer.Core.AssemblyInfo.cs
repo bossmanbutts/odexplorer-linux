31bf1a8d24ed1c3a61f0a916ef66623687ec28c4
milo <milo@localhost>
2026-07-31 22:00:39 -0400

    more UI work

--- src/ODExplorer.Core/obj/Release/net8.0/ODExplorer.Core.AssemblyInfo.cs
+++ src/ODExplorer.Core/obj/Release/net8.0/ODExplorer.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODExplorer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8cde899e37e15b85339bb6ec7d7cce6b8323bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6da9e03e79943201836b9d6cd86193bfe87938")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODExplorer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODExplorer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
