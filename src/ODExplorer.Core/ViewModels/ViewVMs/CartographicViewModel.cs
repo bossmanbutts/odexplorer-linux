d6044235d734bd7c0764e43424a192d90c7aba3c
milo <milo@localhost>
2026-08-16 23:40:27 -0400

    dispatch CurrentState notification on UI thread; add body list to DetailedExo layout

--- src/ODExplorer.Core/ViewModels/ViewVMs/CartographicViewModel.cs
+++ src/ODExplorer.Core/ViewModels/ViewVMs/CartographicViewModel.cs
@@ -30,7 +30,7 @@ namespace ODExplorer.ViewModels.ViewVMs
             this.mainView.OnRouteUpdated += MainView_OnRouteUpdated;
             this.mainView.OnBodyUpdated += MainView_OnBodyUpdated;
             this.mainView.OnBioUpdated += MainView_OnBioUpdated;
-            this.parserStore.OnParserStoreLive += (_, _) => OnPropertyChanged(nameof(CurrentState));
+            this.parserStore.OnParserStoreLive += (_, _) => DispatcherHelper.Invoke(() => OnPropertyChanged(nameof(CurrentState)));
            
             this.mainView.OnSelectedBodyUpdated += MainView_OnSelectedBodyUpdated;
             this.explorationData.OnFSDJump += ExplorationData_OnFSDJump;
